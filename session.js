31f91b2a604182e3cec31f0361556e35682f0d38
Yyun3 <94148707+Yunsilstore@users.noreply.github.com>
2022-02-21 02:26:40 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,13 @@
+{
+
+	"clientID": "rECaI/MZwC5/g6zn6HX/WA==",
+
+	"serverToken": "1@vZ8/4AOYam8Z2IvwIq+4F/WaYW8O3NAHXQBhsWCd9t2DiDfi/nA45EsZT8mOUzAe0hvgYIwKLChofw==",
+
+	"clientToken": "ggTWdANNILJMr7y0lxdGLd4heuA0y/j1d6CUNZwFzZ8=",
+
+	"encKey": "yupmK3ToduHpUvTYkefMAEAMbkN3BAoupYMt6qkHfPw=",
+
+	"macKey": "5an0U6UhaxR5l2kAtwjCp1pwpNpy/Qg7lj2eX4Dm0ag="
+
+}
